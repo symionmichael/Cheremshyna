638e9c44e98dcfcc99b3aba1cb5b4e88a8bc9f8e
Michael <michael.symion@icloud.com>
2026-09-07 21:44:38 +0200

    Gallery
    Add simple gallery

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,3 @@
-// Button animation
-
-
-
 // Navigation scrolling
 const buttons = document.querySelectorAll("nav ul li button");
 
@@ -15,4 +11,31 @@ buttons.forEach(button => {
             block: "start"
         });
     });
+});
+
+const galleryImage = document.getElementById("gallery-image");
+const previousButton = document.querySelector(".gallery-button.previous");
+const nextButton = document.querySelector(".gallery-button.next");
+
+const galleryImages = [
+    { src: "./images/image1.jpg", alt: "Cheremshyna Mini-Hotel" },
+    { src: "./images/image2.jpg", alt: "Room at Cheremshyna Mini-Hotel" },
+    { src: "./images/image3.jpg", alt: "Hotel surroundings in Velyatyno" },
+    { src: "./images/image4.jpg", alt: "Relaxing area at Cheremshyna Mini-Hotel" }
+];
+
+let currentImage = 0;
+
+function showImage(index) {
+    currentImage = (index + galleryImages.length) % galleryImages.length;
+    galleryImage.src = galleryImages[currentImage].src;
+    galleryImage.alt = galleryImages[currentImage].alt;
+}
+
+previousButton.addEventListener("click", () => {
+    showImage(currentImage - 1);
+});
+
+nextButton.addEventListener("click", () => {
+    showImage(currentImage + 1);
 });
